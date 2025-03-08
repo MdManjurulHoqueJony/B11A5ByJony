94e93a62fb09ef6ec2c19e20a97cd60c9cad934e
Md. Manjurul Hoque Jony <kucse03jony@gmail.com>
2025-03-08 13:03:02 +0600

    homepage design

--- scripts/devBoard.js
+++ scripts/devBoard.js
@@ -1,3 +1,9 @@
 document.getElementById('change-theme').addEventListener('click', function () {
     document.body.style.backgroundColor = `hsl(${Math.random() * 360}, 70%, 60%)`;
 })
+
+let date = new Date().toDateString();
+let first = date.substring(0, date.indexOf(" "));
+let rest = date.substring(date.indexOf(" ") + 1);
+console.log(first, rest)
+document.getElementById("date").innerHTML = `${first},<br><strong>${rest}</strong`;
